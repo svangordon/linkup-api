e074a35f0a1b61acae4a5a3debb4e5d4f68e4283
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-08-18 09:55:46 -0600

    gulp improvements
    now caches changes. changed /build to /dist. nodemon now works, w/o that
    annoying endless loop (wasn't specifying that it should only watch /src,
    so it was seeing changes to /dist, re-running, causing changes to /dist,
    etc)

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,45 @@ var gulp        = require('gulp'),
     stylus      = require('gulp-stylus'),
     htmlmin     = require('gulp-htmlmin'),
     del         = require('del'),
-    es          = require('event-stream');
+    es          = require('event-stream'),
+    nodemon     = require('gulp-nodemon'),
+    Cache       = require('gulp-file-cache');
+
+const cache = new Cache();
 
 const paths = {
-  scripts: ['src/**/*.js']
+  scripts: ['./src/**/*.js']
 };
 
 gulp.task('default', ['watch', 'build-server']);
 
 gulp.task('clean', () => {
-  return del(['build'])
+  return del(['build']);
 });
 
-gulp.task('build-server', () => {
-  return gulp.src(paths.scripts)
-      .pipe(sourcemaps.init())
-      .pipe(babel({
-        presets: ['es2015']
-      }))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('build');
+gulp.task('dev', ['compile'], () => {
+  const stream = nodemon({
+    script: 'dist/server.js'
+    , env: {'NODE_ENV': 'development'}
+    , ext: 'js'
+    , watch: 'src'
+    , tasks: ['compile']
+  });
+  return stream;
+})
+
+gulp.task('compile', () => {
+    const stream = gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
+        .pipe(cache.filter())
+        .pipe(babel({
+          presets: ['es2015']
+        }))
+        .pipe(cache.cache())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
+
+    return stream // forces compile to by synchronous
   }
 );
 
